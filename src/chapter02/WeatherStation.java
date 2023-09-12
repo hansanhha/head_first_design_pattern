405bb3fed19b69748611ed836c16d2dc755deae7
hansanhha <kwixs35@gmail.com>
2023-09-13 00:53:44 +0900

    chapter02

---
+++ src/chapter02/WeatherStation.java
@@ -0,0 +1,13 @@
+package chapter02;
+
+public class WeatherStation {
+
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionsDisplay currentDisplay =
+                new CurrentConditionsDisplay(weatherData);
+
+        weatherData.setMeasurements(80, 65, 30.4f);
+    }
+}
